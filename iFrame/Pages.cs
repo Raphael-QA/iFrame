66d766a21a8f2d1428c0718fa5b5a4465306dc7e
Raphael-QA <rafaelqax@gmail.com>
2020-06-26 14:31:12 +0400

    Добавьте файлы проекта.

---
+++ iFrame/Pages.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iFrame
+{
+    class Pages
+    {
+        IWebDriver driver;
+
+        public Pages()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public bool Resualt()
+        {
+            driver.Navigate().GoToUrl("https://designstreet.az/?gclid=EAIaIQobChMIgom5jpmf6gIVS7DtCh37kgQDEAEYASAAEgIMm_D_BwE");
+
+            if (driver.FindElements(By.XPath("//*[text()='XİDMƏTLƏR']")).Count != 1)
+            {
+                driver.Quit();
+                return false;
+
+            }
+            else
+            {
+                driver.Quit();
+                return true;
+            }
+
+        }
+        public bool Resualt2()
+        {
+            driver.Navigate().GoToUrl("https://e.dashamail.ru/?utm_source=google_adwords&utm_medium=cpc&utm_campaign=konkurenti&utm_content=46475941621&keyword=%2Bgetresponse|b&gclid=EAIaIQobChMIsNfXl6Gf6gIVlZ3VCh37cAAOEAEYAiAAEgI5qvD_BwE");
+
+            if (driver.FindElements(By.XPath("//*[text()='Возможности DashaMail']")).Count != 1)
+            {
+                driver.Quit();
+                return false;
+
+            }
+            else
+            {
+                driver.Quit();
+                return true;
+            }
+
+        }
+
+    }
+}
---
+++ iFrame/Test.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iFrame
+{
+    class Test
+    {
+        Pages pages = new Pages();
+        IWebDriver driver;
+        [SetUp]
+        public void SetUp()
+        {
+            
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://someurl.com/");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(6);
+        }
+        [Test]
+        public void TestFirst()
+        {
+            IWebElement frame1 = driver.FindElement(By.XPath("//iframe[@id='master-1']"));
+            driver.SwitchTo().Frame(frame1);
+
+            driver.FindElement(By.XPath("//div[@class='da_']//span[text() = 'Internet URL']/..")).Click();
+
+            IWebElement frame2 = driver.FindElement(By.XPath("//iframe[@id='master-1']"));
+            driver.SwitchTo().Frame(frame2);
+
+            driver.FindElement(By.XPath("//*[@id='e2']/div[3]/div/div/a")).Click();
+
+            Assert.IsTrue(pages.Resualt());
+            driver.Quit();
+        }
+        
+            [Test]
+        public void TestSecond()
+        {
+            IWebElement frame1 = driver.FindElement(By.XPath("//iframe[@id='master-1']"));
+            driver.SwitchTo().Frame(frame1);
+
+            driver.FindElement(By.XPath("//div[@class='da_']//span[text() = 'Get Response']/..")).Click();
+
+            IWebElement frame2 = driver.FindElement(By.XPath("//iframe[@id='master-1']"));
+            driver.SwitchTo().Frame(frame2);
+
+            driver.FindElement(By.XPath("//*[@id='e2']/div[4]/div/div/a/span")).Click();
+
+            Assert.IsTrue(pages.Resualt2());
+            driver.Quit();
+        }
+
+
+
+    }
+}
